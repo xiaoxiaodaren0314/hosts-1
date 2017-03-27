029ef1ad6797bab7ca3df7d640c4924d8c52938f
zhengpu1 <zp500zp@qq.com>
2017-03-27 14:14:50 +0800

    Update hosts及时更新.bat

--- file/hosts及时更新.bat
+++ file/hosts及时更新.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 cd %windir%\system32\drivers\etc
 del hosts
-C:\localdns\wget --no-check-certificate https://raw.githubusercontent.com/WUZHIQIANGX/hosts/master/hosts -O hosts2.txt
+C:\localdns\wget --no-check-certificate https://raw.githubusercontent.com/zhengpu1/hosts/master/1/hosts -O hosts2.txt
 C:\localdns\wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts1.txt
 copy hosts2.txt + hosts1.txt hosts
 ipconfig /flushdns
