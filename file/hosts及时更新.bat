fef89190b94233ddbad40a64941d98e6326eaa55
zhengpu <zp500zp@163.com>
2017-04-17 12:34:45 +0800

    2017年4月14日更新

--- file/hosts及时更新.bat
+++ file/hosts及时更新.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 cd %windir%\system32\drivers\etc
 del hosts
-C:\localdns\wget --no-check-certificate https://raw.githubusercontent.com/zhengpu1/hosts/master/1/hosts -O hosts2.txt
-C:\localdns\wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts1.txt
+C:\wget --no-check-certificate https://raw.githubusercontent.com/zhengpu1/hosts/master/1/hosts -O hosts2.txt
+C:\wget --no-check-certificate https://raw.githubusercontent.com/vokins/yhosts/master/hosts -O hosts1.txt
 copy hosts2.txt + hosts1.txt hosts
 ipconfig /flushdns
 echo 请按任意键进行测试，如取消，请直接关闭本窗口
